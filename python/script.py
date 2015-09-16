5ea8d10f01c8ffd7cc2ba1428cd958133d32f7bf
Adrian Miron <miron_adisor@yahoo.com>
2015-09-16 16:01:12 -0400

    python script added

--- python/script.py
+++ python/script.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python
 def main():
     s = 'Created by Mirona'
+    n = 1
+    v = "This is a %s" %n
+### %s --variable replacement with n value
     print (s)
+    print (v)
 if __name__ == "__main__": main()
 
