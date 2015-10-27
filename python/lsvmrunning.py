fe7ea7660362c3f6a3c0ab9d8124e0d226f884c5
mirona <mirona@mirona.home>
2015-10-27 22:47:31 +0200

    refactoring

--- python/lsvmrunning.py
+++ python/lsvmrunning.py
@@ -3,9 +3,6 @@
 import XenAPI
 import sys
 
-import argparse
-import xmlrpclib
-
 server=sys.argv[1]
 username=sys.argv[2]
 password=sys.argv[3]
@@ -14,7 +11,7 @@ sys.stdout = file('vms.txt', 'w')
 session = XenAPI.Session(server)
 session.xenapi.login_with_password(username,password)
 
- "Session ID: " + session._session
+:wq "Session ID: " + session._session
 for opaque_ref, vm in session.xenapi.VM.get_all_records().items():
     if not(vm["is_a_template"]) and not(vm["is_control_domain"]) and (vm["power_state"] == "Running"):
 	    print vm["name_label"]
