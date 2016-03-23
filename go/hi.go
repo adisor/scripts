4adeee8276952eb012212d4f6e2d2256a9518a1a
Adrian Miron <mirona@localhost.localdomain>
2016-03-23 16:07:18 -0400

    hello world

---
+++ go/hi.go
@@ -0,0 +1,16 @@
+//package declaration
+package main
+
+
+//fmt -shorthand for format-implements formatting for input and output
+import "fmt"
+/*"" double quotes like this are known as a "string literal" which is a type of "expression"*/
+
+// The line that stats with // is known as a comment
+
+func main() {
+    fmt.Println("Hello World")
+}
+
+//This statement is made of three components. First we access another function inside of the fmt package called Println, then we create a new string that contains Hellou World
+
