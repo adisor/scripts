ee7ec4b093f8165d6328a35e86ddb7e518d9e414
mirona <mirona@mirona.home>
2016-05-14 20:30:13 +0300

    excercise 1

---
+++ python/course/basic/ex1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python env
+"""1. Write a Python program to print the following string in a specific format (see the output).
+Sample String : "Twinkle, twinkle, little star, How I wonder what you are! Up above the world so high, Like a diamond in the sky. Twinkle, twinkle, little star, How I wonder what you are" Output :
+
+Twinkle, twinkle, little star,
+	How I wonder what you are! 
+		Up above the world so high,   		
+		Like a diamond in the sky. 
+Twinkle, twinkle, little star, 
+	How I wonder what you are """
+
+print ("Twinkle, twinkle, little star,")
+print ("        How I wonder what you are!")
+print ("	      Up above the world so high,")
+print ("	      Like a diamond in the sky.")
+print ("Twinkle, twinkle, little star,")
+print ("        How I wonder what you are!")
