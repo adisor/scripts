b8ee7dab4423bdb15af1663522970426e979bdac
mirona <mirona@mirona.home>
2016-05-14 20:32:52 +0300

    exercise 1

--- python/course/basic/ex1_a.py
+++ python/course/basic/ex1_a.py
@@ -9,4 +9,9 @@ Twinkle, twinkle, little star,
 Twinkle, twinkle, little star, 
 	How I wonder what you are """
 
-print ("Twinkle, twinkle, little star,\n How I wonder what you are!\n Up above the world so high,\n Like a diamond in the sky.\n Twinkle, twinkle, little star,\n How I wonder what you are\n")
+print ("Twinkle, twinkle, little star,")
+print ("        How I wonder what you are!")
+print ("	      Up above the world so high,")
+print ("	      Like a diamond in the sky.")
+print ("Twinkle, twinkle, little star,")
+print ("        How I wonder what you are!")
