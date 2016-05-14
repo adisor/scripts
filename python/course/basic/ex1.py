517347d76b7f37757cf87217b248ac54c0bf1f73
Adisor <admin@irinamiron.ro>
2016-05-14 20:36:31 +0300

    finished ex1

--- python/course/basic/ex1.py
+++ python/course/basic/ex1.py
@@ -9,4 +9,5 @@ Twinkle, twinkle, little star,
 Twinkle, twinkle, little star, 
 	How I wonder what you are """
 
-print ("Twinkle, twinkle, little star,\n How I wonder what you are!\n Up above the world so high,\n Like a diamond in the sky.\n Twinkle, twinkle, little star,\n How I wonder what you are\n")
+print ("Twinkle, twinkle, little star,\n\tHow I wonder what you are!\n\t\tUp above the world so high,\n\t\tLike a diamond in the sky.\nTwinkle, twinkle, little star,\n\tHow I wonder what you are\n")
+
