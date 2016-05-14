8d7dcf24e946ce5f016b92d53cde94e11cf1e0f6
Adisor <admin@irinamiron.ro>
2016-05-14 22:27:04 +0300

    added ex6

---
+++ python/course/basic/ex6.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python env
+"""Write a Python program which accepts a sequence of comma-separated numbers from user and generate a list and a tuple with those numbers. Go to the editor
+Sample data : 3, 5, 7, 23
+Output :
+List : ['3', ' 5', ' 7', ' 23']
+Tuple : ('3', ' 5', ' 7', ' 23')"""
+
+data = input("Please insert a sequence of comma-separated numbers: ")
+
+list = data.split(",")  
+tuple = tuple(list)
+ 
+print ("List: ", list)
+print ("Tuple:", tuple)
+
