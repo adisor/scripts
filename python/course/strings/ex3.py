a625e30f570237a9f674f3355358b6a33fecb596
Adisor <admin@irinamiron.ro>
2016-05-15 22:54:45 +0300

    strings

--- python/course/strings/ex3.py
+++ python/course/strings/ex3.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python env
-""" Write a Python program to get a string made of the first 2 and the last 2 chars from a given a string. If the string length is less than 2, return instead the empty string. Go to the editor
-Sample String : 'w3resource'
-Expected Result : 'w3ce'
+""" Write a Python program to get a string made of the first 2 and the last 2 chars from a given a string. If the string length is less than 2, return instead the empty string.
 Sample String : 'w3'
-Expected Result : 'w3w3'
-Sample String : ' w'
-Expected Result : Empty String """
+Expected Result : 'w3w3'"""
+
+def string_both_ends(str):
+	if len(str) < 2:
+		return ''
+	return str[0:2] + str[-2:]
+print(string_both_ends('w3resource'))
+print(string_both_ends('w3'))
+print(string_both_ends('w'))
